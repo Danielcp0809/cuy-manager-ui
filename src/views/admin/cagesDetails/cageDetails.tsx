f9120f3a762185208082d193325a7f38fb85dab0
Daniel Cordova <dcordova@udtonline.com>
2024-09-25 00:26:11 -0500

    [Update] Cage details fetching data function enhancements

--- src/views/admin/cagesDetails/cageDetails.tsx
+++ src/views/admin/cagesDetails/cageDetails.tsx
@@ -5,31 +5,33 @@ import useAuthApi from "../../../core/hooks/useAuthApi";
 import { Text, Spinner, Stack, Flex } from "@chakra-ui/react";
 import CageInformation from "./components/CageInformation";
 import CageCounters from "./components/CageCounters";
+import useCustomToast from "../../../core/hooks/useToastNotification";
 
 const CageDetails = () => {
   const { id } = useParams<{ id: string }>();
   const authApi = useAuthApi();
+  const showNotification = useCustomToast();
   const [cageDetails, setCageDetails] = useState<any>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!id) return;
-    let isMounted = true;
     setLoading(true);
     const controller = new AbortController();
     const getData = async () => {
       try {
         const response = await getDataCallback(id, controller);
-        isMounted && setCageDetails(response.data);
+        setCageDetails(response.data);
         setLoading(false);
-      } catch (error) {
+      } catch (error: any) {
+        if(error.code === "ERR_CANCELED") return
+        showNotification("Error", "error", "Ocurrió un error al obtener los detalles de la jaula");
         setLoading(false);
       }
     };
     getData();
 
     return () => {
-      isMounted = false;
       controller.abort();
     };
   }, [id, authApi]);
@@ -40,20 +42,20 @@ const CageDetails = () => {
     });
   };
   
-  if (!cageDetails && !loading) {
-    return (
-      <Flex justifyContent="center" alignItems="center" height="100%">
-        <Text>No se encontraron detalles para de la jaula</Text>
-      </Flex>
-    );  
-  }
-  
   if (loading) {
     return (
       <Flex justifyContent="center" alignItems="center" height="100%" minH="150px">
         <Spinner size="xl" thickness="5px" color="brand.500"/>
       </Flex>
     );
+  }
+
+  if (!cageDetails) {
+    return (
+      <Flex justifyContent="center" alignItems="center" height="100%">
+        <Text>No se encontraron detalles para de la jaula</Text>
+      </Flex>
+    );  
   }
 
   return (
