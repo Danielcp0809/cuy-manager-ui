c75cd5b17cc5e67d9166c463549f9b1d71130b80
Daniel Cordova <jdany0809@hotmail.com>
2024-10-01 18:11:34 -0500

    [Remove] Unused manifest icons

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,7 +15,7 @@ export const authAPI = axios.create({
 })
 
 export const login = async (username: string, password: string) => {
-    console.log(API_URL);
+    console.log(process.env.API_URL);
     const response = await axios.post<IAuthResponse>(`${API_URL}/auth/login`, { username, password });
     return response.data;
 }
