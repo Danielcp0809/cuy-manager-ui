aa9c95b21ab6ed0e54381d2f5803a92b2f5e50af
Daniel Cordova <jdany0809@hotmail.com>
2024-10-01 02:10:25 -0500

    [Add] side bar logo

--- src/components/sidebar/components/Content.tsx
+++ src/components/sidebar/components/Content.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { IRoute } from "../../../interfaces/route.interface";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import { Box, Flex, Stack, Image } from "@chakra-ui/react";
 import Links from "./Links";
+import logo from "../../../assets/img/sidebar/logo02.png";
+import enterpriseLogo from "../../../assets/img/sidebar/enterprise_logo.png";
+import HSeparator from "../../separators/HSeparator";
 
 interface ContentProps {
   routes: IRoute[];
@@ -17,11 +20,19 @@ function Content(props: ContentProps) {
       px="16px"
       borderRadius="30px"
     >
+      <Flex align='center' direction='column'>
+        <Image width="200px" my="20px" src={enterpriseLogo} alt='Logo de empresa' />
+        <HSeparator mb='20px' />
+      </Flex>
       <Stack direction="column" mb="auto" mt="8px">
         <Box ps="20px" pe={{ md: "16px", "2xl": "1px" }}>
           <Links routes={routes} />
         </Box>
       </Stack>
+
+      <Box mt="auto" mb="40px" display="flex" justifyContent="center">
+        <Image width="200px" src={logo}  alt='Cuy manager - sistema de inventario' />
+      </Box>
     </Flex>
   );
 }
