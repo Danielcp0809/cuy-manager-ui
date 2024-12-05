e3e63b9b917d8c46bb3ec1a69197706851d17587
Daniel Cordova <jdany0809@hotmail.com>
2024-12-05 00:24:03 -0500

    [Add] enhance getFormattedDate function to optionally exclude time

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,5 +1,5 @@
 import moment from "moment-timezone";
 
-export const getFormattedDate = (date: any) => {
-    return moment(new Date(Number(date))).format("DD/MM/YYYY HH:mm");
+export const getFormattedDate = (date: any, addTime: boolean = true) => {
+    return moment(new Date(Number(date))).format(addTime ? "DD/MM/YYYY HH:mm" : "DD/MM/YYYY");
 }
