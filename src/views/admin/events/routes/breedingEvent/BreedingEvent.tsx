e3076c2f285093fecf63db8ef9040e398705e885
Daniel Cordova <jdany0809@hotmail.com>
2024-12-05 00:27:22 -0500

    [Add] useForm hook added to control Breeding Event Form

--- src/views/admin/events/routes/breedingEvent/BreedingEvent.tsx
+++ src/views/admin/events/routes/breedingEvent/BreedingEvent.tsx
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BreedingEventForm from "./components/BreedingEventForm";
 import { Box, useDisclosure } from "@chakra-ui/react";
 import Header from "../../components/Header";
+import { useForm } from "react-hook-form";
 
 interface BreedingEventProps {}
 
+export type NewBreedingEventForm = {
+  male: {
+    cage_id: string;
+    category_id: string;
+    quantity: number;
+  };
+  female: {
+    cage_id: string;
+    category_id: string;
+    quantity: number;
+  };
+  cage_id: string;
+  description: string;
+  date: Date;
+  continuous_breeding: boolean;
+};
+
 function BreedingEvent(props: BreedingEventProps) {
   const [loading, setLoading] = React.useState(false);
+  const [isSameCategory, setIsSameCategory] = React.useState<boolean>(false);
+
   const formDisclosure = useDisclosure();
   const { onClose: onFormClose } = formDisclosure;
-  
+
+  const useFormInstance = useForm<NewBreedingEventForm>({ mode: "onChange" });
+  const {
+    watch,
+    formState: { isValid },
+  } = useFormInstance;
+
+  const maleCategoryId = watch("male.category_id");
+  const femaleCategoryId = watch("female.category_id");
+
+  useEffect(() => {
+    if (!maleCategoryId || !femaleCategoryId) {
+      setIsSameCategory(false);
+      return;
+    };
+    setIsSameCategory(maleCategoryId === femaleCategoryId);
+  }, [maleCategoryId, femaleCategoryId]);
+
   const handleClickSave = () => {
+    setLoading(true);
     console.log("Save");
+    console.log(useFormInstance.getValues());
     onFormClose();
+    setLoading(false);
   };
 
   return (
     <Box display="flex" flexDir="column" rowGap={5}>
       <Header
-        form={<BreedingEventForm/>}
+        form={<BreedingEventForm useFormInstance={useFormInstance} isSameCategory={isSameCategory} />}
         formTitle="Nuevo empadre"
         formDisclosure={formDisclosure}
         onSaveForm={handleClickSave}
         loading={loading}
+        isValid={isValid && !isSameCategory}
       />
     </Box>
   );
